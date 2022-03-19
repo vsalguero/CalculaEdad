280fb11867d4970bf460896881c39b1bc3f57a7b
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-03-19 17:34:35 -0600

    feat: adding datePicker

--- app/src/main/java/com/example/calculaedad/MainActivity.kt
+++ app/src/main/java/com/example/calculaedad/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.calculaedad
-
+//import kotlinx.android.synthetic.main.activity_main.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        var etDate = findViewById(R.id.etDate) as EditText
+        etDate.setOnClickListener { showDatePickerDialog() }
+    }
+
+    private fun showDatePickerDialog() {
+        val datePicker = DatePickerFragment { day, month, year -> onDateSelected(day, month, year) }
+        datePicker.show(supportFragmentManager, "datePicker")
+    }
+
+    private fun onDateSelected(day: Int, month: Int, year: Int) {
+        //etDate = findViewById<EditText>(R.id.etDate)
+        var etDate = findViewById(R.id.etDate) as EditText
+        etDate.setText("$day/$month/$year")
     }
 }
