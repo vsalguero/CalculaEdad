2aede8c799b04dded02355e8fe74be37fe00853d
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-03-20 02:18:57 -0600

    feat: create function calculate age

--- app/src/main/java/com/example/calculaedad/MainActivity.kt
+++ app/src/main/java/com/example/calculaedad/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.calculaedad
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.icu.util.Calendar
+import android.net.Uri
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.Integer.parseInt
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +17,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var etDate = findViewById(R.id.etDate) as EditText
         etDate.setOnClickListener { showDatePickerDialog() }
+        var link = findViewById(R.id.link) as TextView
+        link.setOnClickListener{ openLink() }
     }
 
     private fun showDatePickerDialog() {
@@ -21,7 +28,29 @@ class MainActivity : AppCompatActivity() {
 
     private fun onDateSelected(day: Int, month: Int, year: Int) {
         //etDate = findViewById<EditText>(R.id.etDate)
-        var etDate = findViewById(R.id.etDate) as EditText
-        etDate.setText("$day/$month/$year")
+        var etDate = findViewById(R.id.etDate) as EditText;
+        etDate.setText("$day/$month/$year");
+        getAge(year,month,day)
+
+    }
+
+    private fun getAge(year: Int, month: Int, day: Int): String? {
+        val dob: Calendar = Calendar.getInstance()
+        val today: Calendar = Calendar.getInstance()
+        dob.set(year, month, day)
+        var age: Int = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR)
+        if (today.get(Calendar.DAY_OF_YEAR) < dob.get(Calendar.DAY_OF_YEAR)) {
+            age--
+        }
+        val ageInt = age
+
+        var ageText = findViewById(R.id.age) as TextView;
+        ageText.setText("Edad: " + ageInt.toString() + " Años");
+        return ageInt.toString()
+    }
+
+    private fun openLink(){
+        val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.vladi.codes"))
+        startActivity(i)
     }
 }
